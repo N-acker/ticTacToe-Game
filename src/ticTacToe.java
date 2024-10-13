b79822d80864ad49a1068f585e209172e9ffd80a
N-acker <77125698+N-acker@users.noreply.github.com>
2024-10-13 17:46:13 -0400

    Here we've made the game board function and the scanner to take in inputs.

--- src/ticTacToe.java
+++ src/ticTacToe.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ticTacToe {
     public static void main(String[] args) {
 
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'_', '+', '_', '+', '_'},
+                {' ', '|', ' ', '|', ' '},
+                {'_', '+', '_', '+', '_'},
+                {' ', '|', ' ', '|', ' '}};
+
+        printGameBoard(gameBoard);
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a space on the game board (1-9):");
+        int pos = scan.nextInt(); //this takes in the input of the user
+
+        System.out.println(pos);
+
+
+    }
+
+    public static void printGameBoard(char[][] gameBoard) {
+        for(char[] row : gameBoard){
+            for(char column : row){
+                System.out.print(column); //we don't print every column line by line
+            }
+            System.out.println(); //here we print a line after printing each column in the row
+        }
     }
 }
