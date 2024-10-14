b224404f0f71ffab101562df55aedfcb45a2578c
N-acker <77125698+N-acker@users.noreply.github.com>
2024-10-14 18:30:47 -0400

    This is the completed game where a user plays against a CPU. Unfortunatly the CPU simply generates a random number instead of thinking of the best place to place their move. In the next update we'll use some AI to help the CPU do better.

--- src/ticTacToe.java
+++ src/ticTacToe.java
@@ -1,8 +1,10 @@
-import java.util.Arrays;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class ticTacToe {
+
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
+
     public static void main(String[] args) {
 
         char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
@@ -13,23 +15,41 @@ public class ticTacToe {
 
         printGameBoard(gameBoard);
 
-
-
-
+        while(true){
 
             Scanner scan = new Scanner(System.in);
             System.out.println("Enter a space on the game board (1-9):");
-            int playerPosition = scan.nextInt(); //this takes in the input of the user
+            int playerPos = scan.nextInt(); //this takes in the input of the user
+            while(playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)){
+                System.out.println("Position taken! Enter a different position!");
+                playerPos = scan.nextInt();
+            }
 
+            placePiece(gameBoard, playerPos, "player");
 
-            placePiece(gameBoard, playerPosition, "player");
+            String result = checkWinner();
+            if(result.length() > 0){
+                System.out.println(result);
+                break;
+            }
 
             Random rand = new Random();
             int cpuPos = rand.nextInt(9) + 1;
+            while(playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)){
+                cpuPos = rand.nextInt(9) + 1;
+            }
             placePiece(gameBoard, cpuPos, "cpu");
 
             printGameBoard(gameBoard);
 
+            result = checkWinner();
+            if(result.length() > 0){
+                printGameBoard(gameBoard);
+                System.out.println(result);
+                break;
+            }
+            System.out.println(result);
+        }
 
     }
 
@@ -48,8 +68,10 @@ public class ticTacToe {
 
         if(user.equals("player")){
             symbol = 'X';
+            playerPositions.add(pos);
         }else if(user.equals("cpu")){
             symbol = '0';
+            cpuPositions.add(pos);
         }
         switch(pos){
             case 1:
@@ -82,5 +104,40 @@ public class ticTacToe {
             default:
                 break;
         }
+    }
+
+    public static String checkWinner() {
+
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+        List diag1 = Arrays.asList(1, 5, 9);
+        List diag2 = Arrays.asList(7, 5, 3);
+
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(diag1);
+        winning.add(diag2);
+
+        for(List l : winning){
+            if(playerPositions.containsAll(l)){
+                return "Yay you win!";
+            }else if(cpuPositions.containsAll(l)){
+                return "Sorry CPU wins!";
+            }else if(playerPositions.size() + cpuPositions.size() == 9){
+                 return "It's a tie!";
+            }
+        }
+
+        return "";
+
     }
 }
